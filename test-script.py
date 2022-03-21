4938870d094b63f4410d150306792bcb4f549195
Edgar Manrique <edgar.manrique30@gmail.com>
2022-03-21 16:18:52 +0100

    testing python versions

--- test-script.py
+++ test-script.py
@@ -1,1 +1,13 @@
 #this is a test
+import os
+import pandas as pd
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+#load data into a DataFrame object:
+df = pd.DataFrame(data)
+
+print(df)
