6c79bf65b3754b0e24779f28db1ad5f0c96bf45e
Edgar Manrique <edgar.manrique30@gmail.com>
2022-03-28 11:18:33 +0200

    IDE testing

---
+++ class3_IDE.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+#this is the test for the IDE class, using VS code
+
+print(sys.path)
+help("modules")
+days_of_the_week = 7
+def say_hello(recipient):
+    print("Hello world!, {}" .format(recipient))
+    return recipient
